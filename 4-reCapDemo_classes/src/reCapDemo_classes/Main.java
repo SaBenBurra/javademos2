ff9adf64ff2cb3792da4d87956452fcd31e03b5a
Burra <gezerbugra@gmail.com>
2022-09-23 14:39:53 +0300

    reCapDemo_classes project created

---
+++ 4-reCapDemo_classes/src/reCapDemo_classes/Main.java
@@ -0,0 +1,12 @@
+package reCapDemo_classes;
+
+import reCapDemo_classes.*;
+
+public class Main {
+
+  public static void main(String[] args) {
+    DortIslem dortIslem = new DortIslem();
+
+	System.out.println(dortIslem.Topla(3, 2));
+  }
+}
